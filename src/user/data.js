19b6b3e3161a36d7646ada9bd18c5bfd18c7b24a
cdog1526 <mirkwoodlegolas831@gmail.com>
2023-10-08 22:12:59 -0400

    make mass linter

--- src/user/data.js
+++ src/user/data.js
@@ -42,7 +42,6 @@ module.exports = function (User) {
         status: 'offline',
         reputation: 0,
         'email:confirmed': 0,
-        isadmin: false,
     };
 
     User.getUsersFields = async function (uids, fields) {
@@ -158,7 +157,6 @@ module.exports = function (User) {
             User.isAdministrator(callerUID),
             User.isGlobalModerator(callerUID),
         ]);
-        plugins.hooks.fire('action:user.set', { callerUID, isadmin, isAdmin, type: 'set' });
 
         users = await Promise.all(users.map(async (userData, idx) => {
             const _userData = { ...userData };
