d945dc4157bfea0998e47358fddcba720c9156d4
nobodyg0n3 <sriyad@andrew.cmu.edu>
2023-10-27 22:39:02 -0400

    added flow and ran it. 0 errors

--- node_modules/nodebb-plugin-composer-default/controllers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Controllers = {};
-
-Controllers.renderAdminPage = function (req, res) {
-	res.render('admin/plugins/composer-default', {});
-};
-
-module.exports = Controllers;
